37c71056013fd8dfe1c521ec3f3e7ca51ce08e03
Sergey <veselok@gmail.com>
2022-07-15 22:16:22 +0300

    -

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,7 +13,7 @@
             <div class="mb-60 color-white" style="font-size: 50px;">ПРОФИЛЬ</div>
             <div class="color-white flex-column-center font-size-20">
                 <div class="mb-60">Дата оформления подписки: "{{auth()->user()->subscription_start_date}}"</div>
-                <div class="mb-60">Истечение срока подписки: "{{auth()->user()->subscription_end_date}}"</div>
+                <div class="mb-60 @if(!auth()->user()->checkSubscription()) color-red @endif">Истечение срока подписки: "{{auth()->user()->subscription_end_date}}"</div>
             </div>
             <div>
                 <a class="red-button-with-animate" href="{{route('subscription')}}">ОФОРМИТЬ</a>
