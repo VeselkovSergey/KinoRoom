9d9b43c94d4d084c3600e817289f55912188b5df
VeselkovSergey <66443805+VeselkovSergey@users.noreply.github.com>
2022-10-08 11:23:27 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::any('/git-pull', function (Request $request) {
     // sudo -u www-data ssh-keygen - генерим ssh ключи под www-data
-    // sudo chmod 600 /var/www/.ssh/ida_rsa - для работы git pull под www-data
+    // sudo chmod 600 /var/www/.ssh/id_rsa.pub - для работы git pull под www-data
     echo '<pre>' . PHP_EOL;
     echo 'git pull start' . PHP_EOL;
     echo shell_exec('git pull');
