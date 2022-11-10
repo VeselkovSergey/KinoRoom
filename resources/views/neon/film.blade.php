72340aa663fe6af6ca4e1d9c6a0cbf6290a78436
Sergey <veselok@gmail.com>
2022-11-11 01:27:52 +0300

    - play button

--- resources/views/neon/film.blade.php
+++ resources/views/neon/film.blade.php
@@ -95,8 +95,7 @@
             width: 100%;
             height: 100%;
             position: absolute;
-            top: -8px;
-            left: -5px;
+            left: 5px;
         }
     </style>
 
