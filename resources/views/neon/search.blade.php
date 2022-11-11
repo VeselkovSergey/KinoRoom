f9f14eaf51b60ac46e161751f1c84a7f1b9df7fd
Sergey <veselok@gmail.com>
2022-11-11 11:53:59 +0300

    styles

--- resources/views/neon/search.blade.php
+++ resources/views/neon/search.blade.php
@@ -49,7 +49,7 @@
         <div class="flex-center w-100 py-20 pos-sticky top-0 z-1">
             <div class="w-80 pos-rel mr-10">
                 <input style="padding: 16px;" type="text" placeholder="Введите название сериала/фильма/мультфильма" class="text-center search-field">
-                <span class="pos-abs cp" style="right: 10px; top: 35%;" onclick="clearSearchField()">
+                <span class="pos-abs cp" style="right: 10px;top: 22%;background-color: white;padding: 5px;" onclick="clearSearchField()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
