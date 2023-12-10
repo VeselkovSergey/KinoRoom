4cb10a6e79389a0ab96f894a0e44b2d2cb5d4865
Sergey <veselok@gmail.com>
2023-12-11 00:18:29 +0300

    code style

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::get('/analytics-detail/{id}', function () {
 
 Route::get('/', function () {
 //    if (\Illuminate\Support\Facades\Auth::check()) {
-        return redirect(\route('search'));
+    return redirect(\route('search'));
 //    } else {
 //        return redirect(\route('login'));
 //    }
@@ -130,86 +130,86 @@ Route::get('/', function () {
 
 //    Route::group(['middleware' => 'subscription'], function () {
 
-        Route::get('/ref/{refer}', function ($refer) {
-            \App\Models\Refers::create([
-                'refer' => $refer,
-                'client_ip_address' => request()->server('REMOTE_ADDR')
-            ]);
-            return redirect(\route('home'));
-        });
+Route::get('/ref/{refer}', function ($refer) {
+    \App\Models\Refers::create([
+        'refer' => $refer,
+        'client_ip_address' => request()->server('REMOTE_ADDR')
+    ]);
+    return redirect(\route('home'));
+});
 
-        Route::get('/search', function () {
+Route::get('/search', function () {
 //            if (request('neon')) {
-                return view('neon.search');
+    return view('neon.search');
 //            } else {
 //                return view('search');
 //            }
 
-        })->name('search');//->middleware('auth');
+})->name('search');//->middleware('auth');
 
-        Route::get('/film', function () {
+Route::get('/film', function () {
 
-            $apiKey = "4ef0d7355d9ffb5151e987764708ce96";
+    $apiKey = "4ef0d7355d9ffb5151e987764708ce96";
 
-            $filmId = request('id');
-            $isSerial = request('isSerial');
+    $filmId = request('id');
+    $isSerial = request('isSerial');
 
-            $url = 'https://apitmdb.cub.red/3/' . ($isSerial === 'true' ? 'tv' : 'movie') . '/' . $filmId . '?api_key='.$apiKey.'&language=ru';
-            $urlForId = 'https://apitmdb.cub.red/3/'.($isSerial === 'true' ? 'tv' : 'movie').'/'.$filmId.'/external_ids?api_key='.$apiKey.'&language=ru';
+    $url = 'https://apitmdb.cub.red/3/' . ($isSerial === 'true' ? 'tv' : 'movie') . '/' . $filmId . '?api_key=' . $apiKey . '&language=ru';
+    $urlForId = 'https://apitmdb.cub.red/3/' . ($isSerial === 'true' ? 'tv' : 'movie') . '/' . $filmId . '/external_ids?api_key=' . $apiKey . '&language=ru';
 
-            try {
-                $filmInfoRaw = file_get_contents($url);
-                $filmIdsRaw = file_get_contents($urlForId);
-            } catch (Exception $e) {
-                return abort(404);
-            }
+    try {
+        $filmInfoRaw = file_get_contents($url);
+        $filmIdsRaw = file_get_contents($urlForId);
+    } catch (Exception $e) {
+        return abort(404);
+    }
 
-            $filmInfo = json_decode($filmInfoRaw);
-            $filmInfoIds = json_decode($filmIdsRaw);
+    $filmInfo = json_decode($filmInfoRaw);
+    $filmInfoIds = json_decode($filmIdsRaw);
 
-            $filmId = $filmInfo->id;
-            $filmImdbId = $filmInfoIds->imdb_id;
+    $filmId = $filmInfo->id;
+    $filmImdbId = $filmInfoIds->imdb_id;
 
-            $filmTitle = ($filmInfo->title ?? $filmInfo->name) . ' (' . ($filmInfo->release_date ?? $filmInfo->first_air_date) . ')';
-            $filmRawTitle = $filmInfo->title ?? $filmInfo->name;
-            $filmDescription = $filmInfo->overview;
+    $filmTitle = ($filmInfo->title ?? $filmInfo->name) . ' (' . ($filmInfo->release_date ?? $filmInfo->first_air_date) . ')';
+    $filmRawTitle = $filmInfo->title ?? $filmInfo->name;
+    $filmDescription = $filmInfo->overview;
 
-            $filmPosterUrl = $filmInfo->poster_path !== null ? 'https://imagetmdb.com/t/p/w500' . $filmInfo->poster_path : 'https://bpic.588ku.com/back_pic/05/10/88/62598e75d484d19.jpg!/fh/300/quality/90/unsharp/true/compress/true';
-            $filmBackDropUrl = $filmInfo->backdrop_path !== null ? 'https://imagetmdb.com/t/p/w1920_and_h800_multi_faces' . $filmInfo->backdrop_path : null;
+    $filmPosterUrl = $filmInfo->poster_path !== null ? 'https://imagetmdb.com/t/p/w500' . $filmInfo->poster_path : 'https://bpic.588ku.com/back_pic/05/10/88/62598e75d484d19.jpg!/fh/300/quality/90/unsharp/true/compress/true';
+    $filmBackDropUrl = $filmInfo->backdrop_path !== null ? 'https://imagetmdb.com/t/p/w1920_and_h800_multi_faces' . $filmInfo->backdrop_path : null;
 
 //            if (request('neon')) {
-                return view('neon.film', compact('filmRawTitle','filmTitle', 'filmDescription', 'filmPosterUrl', 'filmBackDropUrl', 'filmId', 'isSerial', 'filmImdbId', 'filmInfo'));
+    return view('neon.film', compact('filmRawTitle', 'filmTitle', 'filmDescription', 'filmPosterUrl', 'filmBackDropUrl', 'filmId', 'isSerial', 'filmImdbId', 'filmInfo'));
 //            } else {
 //                return view('film', compact('filmTitle', 'filmDescription', 'filmPosterUrl', 'filmId', 'isSerial'));
 //            }
 
 
-        })->name('film');
+})->name('film');
 
-        Route::get('/get-iframe-content', function () {
-            $ch = curl_init();
+Route::get('/get-iframe-content', function () {
+    $ch = curl_init();
 
 // 2. The URL containing the iframe
 
-            $url = "http://53906.svetacdn.in/tQ7mudXLQUUG/movie/11666";
-            $url = "http:" . request()->get("iframeSrc");
+    $url = "http://53906.svetacdn.in/tQ7mudXLQUUG/movie/11666";
+    $url = "http:" . request()->get("iframeSrc");
 
 // 3. set the options, including the url
 
-            curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $url);
 //curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-            curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
 //    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 //    'Accept-Encoding: gzip, deflate, br',
 //    'Accept-Language: ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
 //    'Cache-Control: no-cache',
 //    'Pragma: no-cache',
 
-                'Referer: http://test.local/',
+        'Referer: http://test.local/',
 
 //    'Sec-Ch-Ua: "Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
 //    'Sec-Ch-Ua-Mobile: ?0',
@@ -219,18 +219,18 @@ Route::get('/', function () {
 //    'Sec-Fetch-Site: cross-site',
 //    'Upgrade-Insecure-Requests: 1',
 //    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.58',
-            ]);
+    ]);
 
 // 4. execute and fetch the resulting HTML output by putting into $output
 
-            $output = curl_exec($ch);
+    $output = curl_exec($ch);
 
 // 5. free up the curl handle
 
-            curl_close($ch);
+    curl_close($ch);
 
-            return $output;
-        })->name('get-iframe-content');//->middleware('auth');
+    return $output;
+})->name('get-iframe-content');//->middleware('auth');
 
 //    });
 
